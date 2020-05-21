043be25f42d9d1e12cd05a69d1bef0f877da1f5c
Kaycheng <kaycheng717@gmail.com>
2020-05-21 21:07:56 +0800

    Finish story action and views

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,29 @@
 class StoriesController < ApplicationController
 
   def index
+    @stories = Story.order(created_at: :desc).includes(:user)
   end
-  
+
+  def new
+    @story = current_user.stories.new
+  end
+
+  def create
+    @story = current_user.stories.new(stories_params)
+    if @story.save
+      redirect_to root_path, notice: "Create new story."
+    else
+      render new_story_path, notice: "Some errors occured."
+    end
+  end
+
+  def show
+    @story = Story.find_by(id: params[:id])
+  end
+
+  private
+
+  def stories_params
+    params.require(:story).permit(:title, :content)
+  end
 end
